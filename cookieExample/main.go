c0364c42d637b5a76fb3b94bf314cbff4f3aeadc
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-03-06 21:21:59 -0600

    Plantilla

--- cookieExample/main.go
+++ cookieExample/main.go
@@ -22,9 +22,12 @@ func obtenerCookie(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteCookie(w http.ResponseWriter, r *http.Request) {
-	chips, _ := r.Cookie("idioma")
-	fmt.Fprint(w, chips)
-	chips.MaxAge = -1
+	oreo1 := &http.Cookie{
+		Name:    "idioma",
+		Value:   "NADAAAAAA",
+		Expires: time.Now().Add(time.Minute * -10),
+	}
+	http.SetCookie(w, oreo1)
 }
 
 func getHTML(w http.ResponseWriter, r *http.Request) {
@@ -54,8 +57,38 @@ func receiveIdiom(w http.ResponseWriter, r *http.Request) {
 }
 
 func mostrarPlantilla(w http.ResponseWriter, r *http.Request) {
+	type product struct {
+		ID          string
+		Product     string
+		Price       int
+		IsFree      bool
+		Description string
+	}
+	var products []product = []product{
+		product{
+			ID:          "!@#$%ˆ",
+			Product:     "Pintura",
+			Description: "Prisma",
+			Price:       129,
+			IsFree:      true,
+		},
+		product{
+			ID:          "12345F",
+			Product:     "Cemento",
+			Description: "Cruz azul",
+			Price:       150,
+			IsFree:      false,
+		},
+		product{
+			ID:          "7584DFT",
+			Product:     "Jabón",
+			Description: "ZOTE",
+			Price:       300,
+			IsFree:      false,
+		},
+	}
 	p, _ := template.ParseFiles("plantilla.html")
-	p.Execute(w, "Ya casi nos vamos")
+	p.Execute(w, products)
 }
 
 func main() {
