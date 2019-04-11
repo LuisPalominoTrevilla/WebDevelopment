b98bf90a07716286313ec5e345852ba8960bb328
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-04-10 20:50:42 -0500

    Finished session practice

--- databasePractice/main.go
+++ databasePractice/main.go
@@ -7,15 +7,47 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/sessions"
 )
 
 var db *sql.DB
+
+// var caja *sessions.CookieStore
+var caja *sessions.FilesystemStore
 
 func init() {
 	var err error
 	db, err = sql.Open("mysql", "root@/golangPractice")
 	if err != nil {
 		log.Fatal("Not able to connect ", err.Error())
+	}
+
+	// caja = sessions.NewCookieStore([]byte("llave"))
+	caja = sessions.NewFilesystemStore("./sesiones", []byte("llavesssfsdgf"))
+}
+
+func activar(w http.ResponseWriter, r *http.Request) {
+	sesion, err := caja.Get(r, "otropues")
+	if err != nil {
+		log.Fatal("error: ", err.Error())
+	}
+	sesion.Values["otropues"] = 3
+	err = sesion.Save(r, w)
+	if err != nil {
+		log.Fatal("error: ", err.Error())
+	}
+}
+
+func revisar(w http.ResponseWriter, r *http.Request) {
+	sesion, err := caja.Get(r, "nombre")
+	if err != nil {
+		log.Fatal("error: ", err.Error())
+	}
+	cok := sesion.Values["nombre"]
+	if cok == nil {
+		fmt.Fprint(w, "Aqui no puedes estar")
+	} else {
+		fmt.Fprint(w, cok)
 	}
 }
 
@@ -44,5 +76,7 @@ func basedatos(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/db", basedatos)
+	http.HandleFunc("/check", revisar)
+	http.HandleFunc("/activate", activar)
 	http.ListenAndServe(":8080", nil)
 }
